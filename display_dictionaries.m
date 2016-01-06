1c8d65390caf20d19c2a07387f95731b6ea60d3b
Cyril Gaudefroy <Cyril@MacBook-Air-de-Cyril.local>
2016-01-06 06:17:00 +0100

    corrections

--- display_dictionaries.m
+++ display_dictionaries.m
@@ -6,13 +6,14 @@ num_freq = size(P4_mat, 1);
 K = size(P4_mat, 2);
 num_dicts = size(P4_mat, 3);
 
-f_range = linspace(0, sr/2, size(spectrogram, 1));
+f_range = linspace(0, sr/2, num_freq);
 
 figure('name', 'Dictionaries')
-subplot1(1, nb_dicts);
-for q = 1:num_dict
+subplot1(1, num_dicts);
+for q = 1:num_dicts
     subplot1(q);
     imagesc(1:K, f_range, 20*log(abs(P4_mat(:, :, q)))); % displayed in magnitude
+    axis([1 K 0 sr/2]);
     set(gca,'YDir','normal');
     drawnow;
 end
