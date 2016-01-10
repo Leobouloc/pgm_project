f1104131fde95e9aea2ed1b93757f663225d5d9f
Cyril Gaudefroy <mail.cyrilgaudefroy@gmail.com>
2016-01-10 18:39:53 +0100

    added file for audio synthesis from mixture weights and complex mixture spectrogram

---
+++ mixture_weights_to_audio.m
@@ -0,0 +1,16 @@
+function [audio1, audio2, sr] = mixture_weights_to_audio(P_final_s1, complex_mixture, sr, g, hop_size, window_size)
+%MIXTURE_WEIGHTS_TO_AUDIO
+
+mixture_weights1 = P_final_s1';
+
+complex_spectrogram1 = mixture_weights1 .* complex_mixture;
+complex_spectrogram2 = (1 - mixture_weights1) .* complex_mixture;
+
+audio1 = idgtreal(complex_spectrogram1, g, hop_size, window_size);
+audio2 = idgtreal(complex_spectrogram2, g, hop_size, window_size);
+
+soundsc(audio1, sr);
+soundsc(audio2, sr);
+
+end
+
